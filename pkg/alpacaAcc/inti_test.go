7582de549dba8a3b6727c0a8f660e00719d5cac2
christhirst <chrisgluon@gmail.com>
2021-09-18 19:39:30 +0000

    gg

--- pkg/alpacaAcc/inti_test.go
+++ pkg/alpacaAcc/inti_test.go
@@ -12,7 +12,7 @@ func TestInit(t *testing.T) {
 		t.Errorf("Expected object not %s", os.Getenv("API_Key_ID")[:4])
 	}
 	if len(os.Getenv("API_Key_ID")[:2]) < 1 {
-		t.Errorf("key is not set %s", os.Getenv("API_Key_ID")[:2])
+		t.Errorf("key is not set %s", os.Getenv("API_Key_ID"))
 	}
 
 }
