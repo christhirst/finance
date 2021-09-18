5aac31b0f5189f9f134951ec14d18095b8a76401
christhirst <chrisgluon@gmail.com>
2021-09-18 17:07:24 +0000

    gfdg

--- pkg/alpacaAcc/inti_test.go
+++ pkg/alpacaAcc/inti_test.go
@@ -3,14 +3,12 @@ package alpacaAcc
 import (
 	"os"
 	"testing"
-
-	"github.com/alpacahq/alpaca-trade-api-go/common"
 )
 
 func TestInit(t *testing.T) {
 	client := Init()
 	if client == nil {
-		t.Errorf("Expected object not %s", os.Setenv(common.EnvApiKeyID, os.Getenv("API_Key_ID")))
+		t.Errorf("Expected object not %s", os.Getenv("API_Key_ID"))
 	}
 
 }
