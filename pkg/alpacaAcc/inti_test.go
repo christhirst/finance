af23d08a890d1cd18e612c082a64eb436fa8bda9
christhirst <chrisgluon@gmail.com>
2021-09-19 08:50:45 +0000

    we

--- pkg/alpacaAcc/inti_test.go
+++ pkg/alpacaAcc/inti_test.go
@@ -12,7 +12,7 @@ func TestInit(t *testing.T) {
 		t.Errorf("Expected object not %s", os.Getenv("GOROOT_1_17_X64"))
 		t.Error(err.Error())
 		t.Error(os.Environ())
-		t.Error(os.Getenv("API_KEY_ID"))
+		t.Error(os.Getenv("API_KEY_ID")[:2])
 	}
 
 }
